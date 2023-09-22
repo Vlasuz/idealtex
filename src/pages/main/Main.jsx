c98cbf68457a65deb4ce06a35085afdd2b3ca1a0
Vlas <vlas.zub@gmail.com>
2023-09-22 13:30:34 +0300

    product card start

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,9 +1,17 @@
+import Card from "../../components/card/Card";
 
 
 export const Main = () => {
+
+    const cardsList = [
+        {
+
+        }
+    ]
+
     return (
         <>
-
+            <Card  />
         </>
     );
 }
