dddd6f4e0d401e843698a2015fa2611a875cb4b1
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-09-26 16:59:47 +0300

    update links

---
+++ src/components/links/Links.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const Links = () => {
+  return (
+    <div class="links">
+        <div class="links__container">
+            <ul class="links__list">
+                <li>
+                    <a href="">
+                        Все для Шиття
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Тканини та Прикладні матеріали
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Блискавка, Нитка, Ґудзик
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Рукоділля та Творчість
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Біжутерія та Ремені
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Новинки
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Акції та Розпродаж
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
