deb8d04250669b89e1d128b9886787562063afed
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-09-26 21:23:29 +0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,22 @@
+import { FooterAboutUs } from "./components/FooterAboutUs/FooterAboutUs";
+import { FooterCategory } from "./components/FooterCategory/FooterCategory";
+import { FooterInfo } from "./components/FooterInfo/FooterInfo";
 
  export const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer class="footer">
+			<div class="footer__container">
+				<div class="footer__top">
+					<FooterAboutUs/>
+
+					<FooterCategory/>
+
+					<FooterInfo/>
+				</div>
+				<div class="footer__copy">
+					<p>© 2023 Idealtex - найкраща швейна фурнітура і тканини оптом All Right Reserved.</p>
+				</div>
+			</div>
+		</footer>
     );
 };
