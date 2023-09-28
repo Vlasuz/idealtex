39032d58d9b7fef771bee0bec1f843e4049da1ec
Vlas <vlas.zub@gmail.com>
2023-09-28 16:36:04 +0300

    fix to continue

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,13 +1,8 @@
-import Card from "../../components/card/Card";
-import {useEffect, useState} from "react";
 import {Links} from "./components/links/Links";
 import {Banner} from '../../components/banner/Banner'
 import {Categories} from "./components/categories/Categories";
 import {Sales} from "./components/sales/Sales";
 import {Text} from "./components/text/Text";
-import {useParams} from "react-router-dom";
-import axios from "axios";
-import {getApiLink} from "../../hooks/getApiLink";
 
 
 export const Main = () => {
