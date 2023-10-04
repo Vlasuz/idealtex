ef7f6fdd9148b91a166fd387e409df3752a3605f
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-04 20:32:41 +0300

    update

--- src/pages/pageNotFound/pageNotFound.jsx
+++ src/pages/pageNotFound/pageNotFound.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
 import {NavLink} from "react-router-dom";
+import { PageNotFoundStyle } from "./pageNotFoundStyle.styled";
 
 const PageNotFound = () => {
     return (
-        <main className={'page'}>
-            <div className="container">
+        <PageNotFoundStyle>
+            <div className="wrapper">
                 <h1>404</h1>
+                <h2>Not Found</h2>
+                <p>The resource requested could not be found on this server!</p>
                 <NavLink to={'/'}>
                     Go to main
                 </NavLink>
             </div>
-        </main>
+        </PageNotFoundStyle>
     );
 };
 
