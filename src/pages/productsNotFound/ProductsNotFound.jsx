3d67d8269e63550413d9c4b1eea6c25d674701c0
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-05 16:53:21 +0300

    create ProductNotFound

---
+++ src/pages/productsNotFound/ProductsNotFound.jsx
@@ -0,0 +1,9 @@
+import { ProductsNotFoundStyle } from "./ProductsNotFound.styled"
+
+export const ProductsNotFound = () => {
+  return (
+    <ProductsNotFoundStyle>
+        <h2>Tоварів не знайдено</h2>
+    </ProductsNotFoundStyle>
+  )
+}
